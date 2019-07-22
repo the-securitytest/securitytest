d48d1e4fbd44eec9d1ba4930348a7ad3e7ab5712
the-securitytest <53171275+the-securitytest@users.noreply.github.com>
2019-07-22 14:01:54 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,3 @@
 alert(document.cookie);
+// Please note that when you are writing JavaScript inside a file, it shouldn’t be enclosed inside a <script> tag. 
+// So consider we created a file cookie.js which contains the code to alert the cookie. 
